15739c356bf9625a24be0d8c5d0868f6209844a9
Gabe <gabiervo@icloud.com>
2025-02-11 17:47:27 -0300

    testing

--- sketch_feb11a/sketch_feb11a.ino
+++ sketch_feb11a/sketch_feb11a.ino
@@ -6,12 +6,21 @@
 #define WHITE SSD1306_WHITE
 #define BLACK SSD1306_BLACK
 
+class Module{
+  private:
+    using func_ptr = void (*)();
+    func_ptr func;
+  public:
+    String moduleName;
+    
+  
+
+};
+
 const int btn1 = 0;
 const int btn2 = 1;
 const int btn3 = 2;
 const int btn4 = 3;
-
-int currColor = WHITE;
 
 Adafruit_SSD1306 oled(128, 64, &Wire, -1);
 
@@ -25,16 +34,9 @@ void setup() {
   pinMode(btn2, INPUT_PULLUP);
   pinMode(btn3, INPUT_PULLUP);
   pinMode(btn4, INPUT_PULLUP);
+
 }
 
 void loop() {
-  int color = WHITE;
-  if(!digitalRead(btn1) || !digitalRead(btn2) || !digitalRead(btn3) || !digitalRead(btn4)){
-    color = BLACK;
-  }
-  if(color != currColor){
-    oled.fillScreen(color);
-    oled.display();
-    currColor = color;
-  } 
+  
 }
